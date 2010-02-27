ecc9263801c821ec724b556199f39a7c68101976
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-27 23:14:07 +0000

    Item1624: fix CSRF fix :/ (thanks Foswiki:Main.PadraigLennon)
    git-svn-id: http://svn.foswiki.org/trunk/TopicClassificationAddOn@6565 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/TopicClassificationAddOn.pm
+++ lib/Foswiki/Contrib/TopicClassificationAddOn.pm
@@ -19,6 +19,6 @@ use strict;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
 $VERSION = '$Rev$';
-$RELEASE = '15 Feb 2010';
+$RELEASE = '27 Feb 2010';
 $SHORTDESCRIPTION = 'Provides framework for organizing  content within a web according to different _types_ of topics (discussion, weblog, reference, etc) and hierarchical "categories". Includes sample topic types and categories. Provides basic framework to design and manage simple Foswiki Applications.';
 
